7b953a34c6e3018f844aeae28fe4683d2bf02627
Hugo Oliveira <hg.oliveira2ds@gmail.com>
2025-10-07 18:50:54 -0300

    feat: implement CreateUser and CheckPassword functions

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -4,13 +4,41 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	ID           uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	ID           uuid.UUID
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
-	Name         string `gorm:"type:varchar(255);not null"`
-	Email        string `gorm:"type:varchar(255);not null;uniqueIndex"`
-	PasswordHash string `gorm:"type:text;not null"`
+	Name         string
+	Email        string
+	PasswordHash []byte
+}
+
+func CheckPassword(u *User, password string) bool {
+	passwordBytes := []byte(password)
+	err := bcrypt.CompareHashAndPassword(u.PasswordHash, passwordBytes)
+
+	return err == nil
+}
+
+func CreateUser(name string, email string, password string) *User {
+	id := uuid.New()
+	createdAt := time.Now()
+	updatedAt := time.Now()
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		panic("Erro ao gerar o hash da senha: " + err.Error())
+	}
+
+	return &User{
+		ID:           id,
+		CreatedAt:    createdAt,
+		UpdatedAt:    updatedAt,
+		Name:         name,
+		Email:        email,
+		PasswordHash: passwordHash,
+	}
 }
